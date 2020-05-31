47e4b573e95e47462b10fd4efc9e8c68096ff72a
guptachandan <chandan4286@gmail.com>
2020-05-31 05:22:56 -0300

    Added index,reg,login/page

---
+++ inc/footer.php
@@ -0,0 +1,13 @@
+<?php
+if(!defined('__CONFIG__')) {
+exit('You do not have a config file');
+	}
+?>
+
+
+<!-- jQuery is required -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	
+<!-- UIkit JS -->
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.3/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.3/dist/js/uikit-icons.min.js"></script>
